34d025d6db0090505c22e31a1a966c31f86b6006
Ibrahim M. Ibrahimli <IbrahimMi@code.local>
2019-09-13 18:43:21 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// let canvas = document.getElementById('c1');
+// let ctx = canvas.getContext('2d');
+//  let myColor='red'
+
+// document.getElementById('color').oninput=function(e){
+
+//            myColor=this.value;
+
+// }
+
+// canvas.onmousedown=function(e){
+    
+    
+    
+//     canvas.onmousemove=function(e){
+//         let x=e.offsetX;
+//         let y=e.offsetY;
+        
+//     ctx.fillStyle=myColor;
+//         ctx.fillRect(x-5,y-5,10,10);
+        
+//      canvas.onmouseup=function(){
+
+//         canvas.onmousemove=null;
+//      }
+        
+        
+//     }
+
+
+// }
+
+
+function add(){
+let input = document.getElementById('input').value;
+let text = document.getElementById('text');
+
+let li = document.createElement('li');
+li.classList.add('list-group-item', 'list-group-item-action');
+let btn=document.createElement("button");
+btn.textContent='Remove';
+
+
+btn.onclick=function(){
+
+    removeLi(btn);
+}
+
+li.onclick=function(){
+
+    addToP(li);
+    
+}
+
+li.innerHTML=input;
+
+
+text.appendChild(li);
+li.appendChild(btn);
+
+
+}
+
+function addToP(e){
+    let p=document.getElementById('p1');
+    
+    p.innerHTML+= '</br>' + e.innerHTML;
+    
+    
+    }
+
+
+    function removeLi(e){
+
+let LiElement=e.parentElement;
+let ulElement=LiElement.parentElement;
+
+ulElement.removeChild(LiElement);
+
+
+}
